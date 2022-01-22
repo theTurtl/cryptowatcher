03ffa8d9f50e59914e0fc7b31a9de1497baa7f91
theTurtl <addimanni@gmail.com>
2022-01-22 17:36:23 +0100

    Intsalled react-router-dom, working commit.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 
 const App = () => {
-  return <div>
-      <h1>Test</h1>
-  </div>;
+  return (
+  <div>
+      <div className='app'>
+          <div className='navbar'>
+
+          </div>
+          <div className='main'>
+              
+          </div>
+          <div className='footer'>
+
+          </div>
+      </div>
+  </div>)
+  ;
 };
 
 export default App;
