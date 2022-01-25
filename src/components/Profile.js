683826910535c9925a0ba7fb37aa99a24891ea0a
theTurtl <addimanni@gmail.com>
2022-01-25 01:42:51 +0100

    Tired, going to bed. Commiting all changes.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Profile = () => {
   return <div className='profileView'>
+      <div className='profileBoxContent'>
+          <div className='profilePic'></div>
+          <p>Adrian</p>
+          </div>
       
   </div>;
 };
